1c6f10050e79301c667b59c932b4a604ea1a2734
Gissio <mressl@umich.edu>
2024-05-24 21:35:56 -0300

    2.0rc5

--- platform.io/src/system.h
+++ platform.io/src/system.h
@@ -16,7 +16,7 @@
 
 #define FIRMWARE_AUTHOR "Gissio"
 #define FIRMWARE_NAME "Rad Pro"
-#define FIRMWARE_VERSION "2.0rc5-test11"
+#define FIRMWARE_VERSION "2.0rc5-test12"
 
 extern const View statisticsView;
 
