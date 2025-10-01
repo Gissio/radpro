580b2ae5007480d57f14d2a3a4ea002c2c777536
Gissio <mressl@umich.edu>
2025-10-01 19:29:25 +0000

    3.0.2

--- tools/radpro-tool.py
+++ tools/radpro-tool.py
@@ -329,7 +329,9 @@ def download_datalog(io,
         log_warning(f'could not write {path}')
 
 
-def send_http_request(url, method='get', json=None, data=None, headers=None):
+def send_http_request(url, method='get', json=None, data=None):
+    headers = {'User-Agent': 'radpro-tool/' + radpro_tool_version}
+
     try:
         if method == 'get':
             requests.get(url=url, timeout=60, headers=headers)
@@ -412,16 +414,16 @@ def stream_datalog(io, args):
                     f'?AID={args.submit_gmcmap[0]}' + f'&GID={args.submit_gmcmap[1]}' + \
                     f'&CPM={cpm:.0f}' + f'&ACPM={cpm:.3f}' + \
                     f'&uSV={uSvH:.3f}'
-                send_http_request(url, 'get', headers={'User-Agent': 'radpro-tool/' + radpro_tool_version})
-                print(f'GMCMap submission - DateTime: {curr_datetime}, CPM: {cpm:.3f}, uSv/h: {uSvH:.3f}')
+                send_http_request(url, 'get')
+                print(f'GMCMap submission: {curr_datetime} CPM:{cpm:.3f} uSv/h:{uSvH:.3f}')
 
             if args.submit_radmon != None and cpm != None:
                 url = 'https://radmon.org/radmon.php?function=submit' + \
                     f'&user={args.submit_radmon[0]}' + \
                     f'&password={args.submit_radmon[1]}' + \
                     f'&value={cpm:.3f}' + '&unit=CPM'
-                send_http_request(url, headers={'User-Agent': 'radpro-tool/' + radpro_tool_version})
-                print(f'Radmon submission - DateTime: {curr_datetime}, CPM: {cpm:.3f}, uSv/h: {uSvH:.3f}')
+                send_http_request(url)
+                print(f'Radmon submission: {curr_datetime} CPM:{cpm:.3f} uSv/h:{uSvH:.3f}')
 
             if args.submit_safecast != None and cpm != None:
                 url = 'https://api.safecast.org/measurements.json?api_key=' + \
@@ -435,8 +437,8 @@ def stream_datalog(io, args):
                     'longitude': str(args.safecast_longitude),
                     'height': str(args.safecast_height),
                 }
-                send_http_request(url, 'post', json=json, headers={'User-Agent': 'radpro-tool/' + radpro_tool_version})
-                print(f'Safecast submission - DateTime: {curr_datetime}, CPM: {cpm:.3f}, uSv/h: {uSvH:.3f}')
+                send_http_request(url, 'post', json=json)
+                print(f'Safecast submission: {curr_datetime} CPM:{cpm:.3f} uSv/h:{uSvH:.3f}')
 
         # Wait for next measurement
         next_event += args.period
@@ -483,7 +485,7 @@ def main():
                         help='download only data log records older than a certain ISO 8601 date and time (e.g. "2000-01-01" or "2000-01-01T12:00:00")')
     parser.add_argument('--download-datalog-max-record-num',
                         dest='datalog_max_record_num',
-                        help='limi the number of data log records to download')
+                        help='limit the number of data log records to download')
     parser.add_argument('--log-pulsedata',
                         dest='pulsedata_file',
                         help='log live pulse data to a .csv file')
@@ -511,15 +513,15 @@ def main():
     parser.add_argument('--safecast-latitude',
                         type=float,
                         default=0.0,
-                        help='latitude for Safecast submissions (default: 0.0)')
+                        help='decimal latitude for Safecast submissions (default: 0.0)')
     parser.add_argument('--safecast-longitude',
                         type=float,
                         default=0.0,
-                        help='longitude for Safecast submissions (default: 0.0)')
+                        help='decimal longitude for Safecast submissions (default: 0.0)')
     parser.add_argument('--safecast-height',
                         type=float,
                         default=0.0,
-                        help='height/altitude for Safecast submissions (default: 0.0)')
+                        help='height (altitude) in meters for Safecast submissions (default: 0.0)')
     parser.add_argument('--log-randomdata',
                         dest='randomdata_file',
                         help='log live randomly generated data to a binary file')
